121ad87f873e29dc08dd89fd29c952a806eb280b
qbnyp <moneycloudy@naver.com>
2024-01-13 01:21:42 +0900

    싸이월드 수정중, js js 내장함수 타이머

--- 1week/class/03-javascript/05-function.js
+++ 1week/class/03-javascript/05-function.js
@@ -1,29 +1,41 @@
+let isStarted = false; //타이머 연속 실행 방지하기위한 변수
 
-const random = () => { 
-    let result = String ( Math.floor(Math.random() * 1000000) ).padStart(6,"0")
-    console.log(result)
-    document.getElementById("target").innerText = result
-    document.getElementById("target").style.color = `#${result}`
-
-    let time = 10
-
-    setInterval(function(){
-        if(time >= 0){
-            let min = Math.floor( time / 60)
-            let sec = String(time % 60).padStart(2, "0");
-            console.log(min + ":" + sec)
-            document.getElementById("timer").innerText = min + ":" + sec
+let auth = () => {
+    
+    if(isStarted === false){ 
+        // 타이머가 작동중이 아닐때 실행한다
+            isStarted = true  // 타이머 실행중! 체크!
+            document.getElementById("btn").disabled = false
+            let result = String ( Math.floor(Math.random() * 1000000) ).padStart(6,"0")
+            console.log(result)
+            document.getElementById("target").innerText = result
+            document.getElementById("target").style.color = `#${result}`
+        
+            let time = 10
+            let timer
             
-            time = time - 1
-        }
-
-        if(time == 0){
-            const btn = document.getElementById("end");
-            btn.disabled = true;
-        }
-    },1000)
+            timer = setInterval(function(){
+                if(time >= 0){
+                    let min = Math.floor( time / 60)
+                    let sec = String(time % 60).padStart(2, "0");
+                    console.log(min + ":" + sec)
+                    document.getElementById("timer").innerText = min + ":" + sec
+                    time = time - 1
+                }
+        
+                else{
+                    document.getElementById("btn").disabled=true
+                    isStarted = false;
+                    console.log("작동중")
+                    clearInterval(timer)
+                }
+            },1000)
+        
+    }else{
 
     }
+}
+
 
 
     
--- 1week/cyworld/game.js
+++ 1week/cyworld/game.js
@@ -1,16 +1,19 @@
 const startWord = () => {
-    let myword = document.getElementById("myword").value
-    let word = document.getElementById("word").innerText
+    let myword = document.getElementById("myword").value //내가쓴 단어
+    let word = document.getElementById("word").innerText //제시어
 
-    let lastword = [word.length - 1]
+    let lastword = word[word.length - 1] // 제시어의 마지막 단어
     let firstword = myword[0]
+
+    console.log(lastword)
+    console.log(firstword)
 
     if(lastword === firstword) {
         document.getElementById("result").innerText = "정답입니다."
         document.getElementById("word").innerText = myword
         document.getElementById("myword").value = ""
     }else { 
-        document.getElementById("result").innerText = "!땡"
+        document.getElementById("result").innerText = "땡!"
         document.getElementById("myword").value = ""
     }
 
