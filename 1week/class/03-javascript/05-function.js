2c120f7ba7c6610f4e79ef9f9125e2e23961d410
qbnyp <moneycloudy@naver.com>
2024-01-12 22:28:25 +0900

    0112

--- 1week/class/03-javascript/05-function.js
+++ 1week/class/03-javascript/05-function.js
@@ -5,5 +5,25 @@ const random = () => {
     document.getElementById("target").innerText = result
     document.getElementById("target").style.color = `#${result}`
 
+    let time = 10
+
+    setInterval(function(){
+        if(time >= 0){
+            let min = Math.floor( time / 60)
+            let sec = String(time % 60).padStart(2, "0");
+            console.log(min + ":" + sec)
+            document.getElementById("timer").innerText = min + ":" + sec
+            
+            time = time - 1
+        }
+
+        if(time == 0){
+            const btn = document.getElementById("end");
+            btn.disabled = true;
+        }
+    },1000)
 
     }
+
+
+    
