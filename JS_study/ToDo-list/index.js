1672627f5b865136a994c34f7dbf4cfd7c3bc4bd
qbnyp <moneycloudy@naver.com>
2024-02-21 08:53:11 +0900

    fetch, then , json파일 가져오기 공부

--- JS_study/ToDo-list/index.js
+++ JS_study/ToDo-list/index.js
@@ -1,24 +1,37 @@
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
-const createTodo = function () {
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
-  console.log(newBtn);
 
+  //클래스 complete 이름으로 생성
+  //토글이기에 켰다 껏다가능
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
-    //클래스 complete 이름으로 생성
-    //토글이기에 켰다 껏다가능
+    saveItemsFn();
   });
 
+  //더블클릭시 삭제
   newLi.addEventListener("dblclick", () => {
     newLi.remove();
-    //더블클릭시 삭제
+    saveItemsFn();
   });
 
-  newSpan.textContent = todoInput.value; //CONTENT라 <SPAN>!!!</SPAN>안에 들어가는거같음
+  //정상적이지 않은값이면 그냥 넘어감
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents; //CONTENT라 <SPAN>!!!</SPAN>안에 들어가는거같음
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
@@ -39,10 +52,12 @@ const deleteAll = function () {
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
   }
+  saveItemsFn();
 };
 
 const saveItemsFn = function () {
   const saveItems = [];
+
   for (let i = 0; i < todoList.children.length; i++) {
     const todoObj = {
       contents: todoList.children[i].querySelector("span").textContent,
@@ -50,5 +65,62 @@ const saveItemsFn = function () {
     };
     saveItems.push(todoObj);
   }
-  console.log(saveItems);
+
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+
+  //localStorage에 값을 넣을려면 string으로 줘야해서 JSON.stringify(값)을 하면 string으로 파싱해서 넣을수있음.
+
+  //   console.log(typeof JSON.stringify(saveItems));
 };
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
+
+const weatherSearch = function (position) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=d5adc8ce05e9e4cd506e8886305da11e`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].description);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const accessToGeo = function (position) {
+  const positionObj = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
+
+askForLocation();
+
+// const promiseTest = function () {
+//   return new Promise((resolver, reject) => {
+//     setTimeout(() => {
+//       // resolver("success");
+//       reject("error");
+//     }, 5000);
+//   });
+// };
+
+// promiseTest().then((res) => {
+//   console.log(res);
+// });
