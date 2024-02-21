9e48c8617b470e2142e7e7f9f1007a569d4388e3
qbnyp <moneycloudy@naver.com>
2024-02-21 19:22:30 +0900

    react css적용, 실행해보기

--- frontend/class/pages/_app.js
+++ frontend/class/pages/_app.js
@@ -1,3 +1,5 @@
+//모든페이지의 공통설정들을 여기서 진행함
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
---
+++ frontend/class/pages/section01/01-01-example/index.js
@@ -0,0 +1,13 @@
+export default function 나만의페이지() {
+  //js
+
+  const aaa = 3;
+
+  return (
+    //html
+    <div>
+      <div>철수</div>
+      <button>클릭하세요!!</button>
+    </div>
+  );
+}
