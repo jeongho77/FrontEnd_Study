6669751f1f99f52974e8544dfe48fbc49a31e2b5
qbnyp <moneycloudy@naver.com>
2024-02-29 14:01:25 +0900

    dynamic routing 학습!

---
+++ React_study/class/pages/section05/05-04-dynamic-routing-board-query-moved/[qqq]/index.js
@@ -0,0 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+const FETCH_BOARD = gql`
+  query fetchBoard($number: Int) {
+    fetchBoard(number: $number) {
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const router = useRouter();
+  console.log(router.query.qqq);
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { number: Number(router.query.qqq) },
+  });
+
+  return (
+    <div>
+      <div>{router.query.qqq}번 페이지 이동이 완료되었습니다.</div>
+      <div>작성자 : {data && data.fetchBoard?.writer}</div>
+      {/* 일단 data를 받아오고 있는동안에 data값을 보여주고 data가 오면 값을 보여주기 */}
+      <div>제목 : {data?.fetchBoard?.title}</div>
+      {/* 옵셔널 체이닝 optional-chaining이라고 함 */}
+      <div>내용 : {data ? data.fetchBoard?.contents : "로딩중입니다!!"}</div>
+    </div>
+  );
+}
---
+++ React_study/class/pages/section05/05-04-dynamic-routing-board-query-moved/index.js
@@ -0,0 +1,5 @@
+import { useQuery } from "@apollo/client";
+
+export default function StaticRoutingMovedPage() {
+  return <div>페이지 이동이 완료되었습니다.</div>;
+}
