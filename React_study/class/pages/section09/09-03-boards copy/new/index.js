38c9f4c13cfbff6a411b11c232851b0a320d2f49
qbnyp <moneycloudy@naver.com>
2024-07-14 00:00:18 +0900

    09-04 완료

--- React_study/class/pages/section09/09-03-boards copy/new/index.js
+++
@@ -1,5 +0,0 @@
-import BoardWrite from "../../../../src/components/units/board/09-write/BoardWrite.container";
-
-export default function BoardsNewPage() {
-  return <BoardWrite isEdit={false} />
-}
