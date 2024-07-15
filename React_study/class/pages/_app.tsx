cc6d1f0460ab5c69bfcdcacc0721a7f056f9d1f5
qbnyp <moneycloudy@naver.com>
2024-07-15 17:34:43 +0900

    TS 설치

--- React_study/class/pages/_app.tsx
+++ React_study/class/pages/_app.tsx
@@ -1,8 +1,9 @@
 // 모든페이지의 공통설정들을 여기서 진행함
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-export default function App({ Component }) {
+export default function App({ Component }: AppProps ) {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해놓기 => 나중에 더 자세히 알아보기
